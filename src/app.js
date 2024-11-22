15f5a68000d2cdc7dc7fd774e1203e82cb4cf32e
brianArruk <brianarruk22@gmail.com>
2024-11-21 22:32:02 -0300

    first commit :zap:

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const morgan = require("morgan")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+
+const app = express()
+
+
+module.exports = app
